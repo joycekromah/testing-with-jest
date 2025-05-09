146e73c430f7fa23d50da1f200513030a6bf4028
joycekromah <joycekromah@hotmail.com>
2025-05-09 16:56:35 +0200

    Webpack mode fel rad

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
+    mode: 'production',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        mode: 'production',
+        filename: 'main.js'
     },
     plugins: [
         new CopyWebpackPlugin({
